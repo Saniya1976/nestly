13e1b8a89190577310e22f3e1e2d9e7be2b5f06a
Saniya1976 <saniyakumari1976@gmail.com>
2025-08-30 00:04:11 +0530

    create post components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import ModeToggle from "../../components/ModeToggle";
 
 export default function Home() {
   return (
-    <div>
-    
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+    <div className="lg:col-span-6">
+
+    </div>
       </div>
   );
 }
