27ab30489e52c97730d1ac5a2c7943308349b6e6
Saniya1976 <saniyakumari1976@gmail.com>
2025-09-07 21:23:08 +0530

    completed profile.actions.ts

--- src/actions/profile.action.ts
+++ src/actions/profile.action.ts
@@ -4,6 +4,7 @@
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { getDbUserId } from "./user.action";
+import { auth } from "@clerk/nextjs/server";
 export async function getProfile(username:string){
     try {
         const profile=await prisma.user.findUnique({
@@ -132,5 +133,49 @@ export async function getUserLikedPosts(userId:string){
     } catch (error) {
          console.error("Error fetching liked posts:", error);
     throw new Error("Failed to fetch liked posts");
+    }
+}
+export async function updateProfile({formData }: {formData: FormData }) {
+try {
+    const { userId: clerkId } = await auth();
+    if (!clerkId) throw new Error("Unauthorized");
+    const name = formData.get("name") as string;
+    const bio = formData.get("bio") as string;
+    const location = formData.get("location") as string;
+    const website = formData.get("website") as string;
+
+    const user = await prisma.user.update({
+      where: { clerkId },
+      data: {
+        name,
+        bio,
+        location,
+        website,
+      },
+    });
+
+    revalidatePath("/profile");
+    return {success:true, user};
+} catch (error) {
+      console.error("Error updating profile:", error);
+      return { success: false, error: "Failed to update profile" };
+}
+}
+export async function isFollowing(userId:string){
+    try {
+       const currentUserId = await getDbUserId();
+       if (!currentUserId) return false; 
+       const follow=await prisma.follows.findUnique({
+        where:{
+            followerId_followingId:{
+                followerId: currentUserId,
+                followingId: userId,
+            }
+        }
+       })
+       return !!follow;
+    } catch (error) {
+          console.error("Error checking follow status:", error);
+          return false;
     }
 }
